5bdcf34607551af53fecfe0c79f07bd0c247e8a8
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2025-03-30 21:55:15 -0700

    bugged dda

--- src/display.c
+++ src/display.c
@@ -18,27 +18,46 @@ void draw_ceiling_and_floor(App *app) {
 
 float trace_ray_to_wall(Player *player, char map[MAP_SIZE][MAP_SIZE],
                         float angle) {
-  const float step_size = 0.1f;
-  int x;
-  int y;
-  float dir_x = cos(angle);
-  float dir_y = sin(angle);
-  char hit_bool = 0;
-  float step = 0;
+  float cos_angle = cos(angle);
+  float sin_angle = sin(angle);
 
-  while (!hit_bool) {
-    x = (int)(player->position.x + step * dir_x);
-    y = (int)(player->position.y + step * dir_y);
-    /*printf("x: %d, y: %d, dir_x: %f, dir_y: %f, step: %f, theta: %f\n", x, y,
-           dir_x, dir_y, step, angle);*/
-    if (map[y][x] == 1)
-      hit_bool = 1;
-    step += step_size;
+  float x = player->position.x;
+  float y = player->position.y;
+
+  int hit = 0;
+  float distance_to_wall = 0;
+
+  while (!hit) {
+    // printf("cos: %f, sin: %f\n", cos_angle, sin_angle);
+    float dist_x = cos_angle >= 0 ? (int)x + 1 - x : (int)x - 1 + x;
+    float dist_y = sin_angle >= 0 ? (int)y + 1 - y : (int)y - 1 + y;
+
+    float ray_dist_v = fabs(cos_angle != 0 ? dist_x / cos_angle : MAXFLOAT);
+    float ray_dist_h = fabs(sin_angle != 0 ? dist_y / sin_angle : MAXFLOAT);
+
+    /*printf("dist_x: %f, dist_y: %f, ray_dist_v: %f, ray_dist_h: %f\n", dist_x,
+           dist_y, ray_dist_v, ray_dist_h);*/
+
+    if (ray_dist_h < ray_dist_v) {
+      x = x + cos_angle * ray_dist_h;
+      y = sin_angle >= 0 ? (int)y + 1 : (int)y;
+      distance_to_wall += ray_dist_h;
+      // printf("horizontal - x: %f, y: %f\n", x, y);
+      if (map[(int)y][(int)x] == 1) {
+        hit = 1;
+      }
+    } else {
+      x = cos_angle >= 0 ? (int)x + 1 : (int)x;
+      y = y + sin_angle * ray_dist_v;
+      distance_to_wall += ray_dist_v;
+      // printf("vertical - x: %f, y: %f\n", x, y);
+      if (map[(int)y][(int)x] == 1) {
+        hit = 1;
+      }
+    }
   }
-  /*printf(
-      "HIT A WALL! x: %d, y: %d, dir_x: %f, dir_y: %f, step: %f, theta: %f\n",
-      x, y, dir_x, dir_y, step, angle);*/
-  return step;
+
+  return distance_to_wall;
 }
 
 void draw_wall_column(App *app, int column_index, float distance) {
