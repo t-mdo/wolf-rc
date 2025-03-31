25e5ba4d6b4769261a567468e4a7880b0e097356
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2025-03-30 21:52:29 -0700

    Fix minimap fov origin point

--- src/minimap.c
+++ src/minimap.c
@@ -64,22 +64,22 @@ void draw_minimap(App *app, Player *player, char map[MAP_SIZE][MAP_SIZE]) {
   float fov_angle_1 = player->view_angle - FOV_ANGLE / 2;
   float fov_angle_2 = player->view_angle + FOV_ANGLE / 2;
 
-  printf("%f %f %f\n", player->view_angle, fov_angle_1, fov_angle_2);
-  SDL_Point fov_points[3] = {{
-                                 .x = x_rect_player_position +
-                                      cos(fov_angle_1) * MINIMAP_CELL_SIZE * 4,
-                                 .y = y_rect_player_position +
-                                      sin(fov_angle_1) * MINIMAP_CELL_SIZE * 4,
-                             },
-                             {
-                                 .x = x_rect_player_position,
-                                 .y = y_rect_player_position,
-                             },
-                             {
-                                 .x = x_rect_player_position +
-                                      cos(fov_angle_2) * MINIMAP_CELL_SIZE * 4,
-                                 .y = y_rect_player_position +
-                                      sin(fov_angle_2) * MINIMAP_CELL_SIZE * 4,
-                             }};
+  SDL_Point fov_points[3] = {
+      {
+          .x =
+              x_rect_player_position + cos(fov_angle_1) * MINIMAP_CELL_SIZE * 4,
+          .y =
+              y_rect_player_position + sin(fov_angle_1) * MINIMAP_CELL_SIZE * 4,
+      },
+      {
+          .x = x_rect_player_position + PLAYER_SQUARE_SIZE / 2,
+          .y = y_rect_player_position + PLAYER_SQUARE_SIZE / 2,
+      },
+      {
+          .x =
+              x_rect_player_position + cos(fov_angle_2) * MINIMAP_CELL_SIZE * 4,
+          .y =
+              y_rect_player_position + sin(fov_angle_2) * MINIMAP_CELL_SIZE * 4,
+      }};
   SDL_RenderDrawLines(app->renderer, fov_points, 3);
 }
